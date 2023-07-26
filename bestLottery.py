1f52d35ec34e897f38ed7a89258ce920a0725f04
Leo Larocque <117418726+LeoLarocque@users.noreply.github.com>
2023-07-26 14:29:20 -0600

    Add files via upload

---
+++ bestLottery.py
@@ -0,0 +1,28 @@
+import random
+x = 0
+num = ""
+randNum = random.randint(1, 100)
+name = input("Enter name: ")
+print("Hello, " + name + "!")
+nameLower = name.lower()
+if nameLower == "leo":
+    input("Enter number: ")
+    print("Correct Guess!")
+else:
+    while not num == randNum:
+        x += 1
+        num = input("Enter number: ")
+        if int(num) > randNum:
+            print(str(x) + "ST Guess: to high.")
+        elif int(num) < randNum:
+            print(str(x) + "ST Guess: to low.")
+        if int(num) == randNum:
+            print("Correct! It took you " + str(x) + " tries to guess the number.")
+            break
+    print("The number was " + str(randNum) + ".")
+def function():
+    fileName = name + ".txt"
+    f = open(fileName, "w")
+    f.write("Name = " + str(name) + "\nNumber of tries = " + str(x) + "\nThe number = " + str(randNum))
+    f.close()
+function()
